d4c375f9aeb477292c21de45cb229f5837876203
lkpetrich <petrich@panix.com>
2022-10-24 17:54:10 -0700

    Rewrote the creation of the image buffer
    Image-buffer creation rewritten because methods lockFocusFlipped and unlockFocus are now deprecated. Instead creating a NSImage with imageWithSize:flipped:drawingHandler: and moving the drawing code into a block.

--- Sudoku Solver/SudokuBkgd.mm
+++ Sudoku Solver/SudokuBkgd.mm
@@ -39,70 +39,70 @@ inline NSColor *GetBkgdColor(CGFloat Hue)
     NSColor *OddOddColor = GetBkgdColor(0.5);
     NSColor *LineColor = [NSColor grayColor];
     
-    // Use offscreen buffer
-    NSImage *BkgdImg = [[NSImage alloc] initWithSize:NSMakeSize(BoardSideLen, BoardSideLen)];
-    [BkgdImg lockFocusFlipped:YES];
+    BOOL (^BackgroundDrawer)(NSRect) = ^BOOL (NSRect DestRect) {
+        // Draw!
+        for (uint ir=0; ir<SideLength; ir++)
+        {
+            // The cell backgrounds
+            for (uint ic=0; ic<SideLength; ic++)
+            {
+                // the coordinates
+                uint ibr = ir / Rows;
+                uint ibc = ic / Cols;
+                
+                // The color
+                bool CellParity = ((ir+ic) % 2) == 0;
+                bool BlockParity = ((ibr+ibc) % 2) == 0;
+                NSColor *BkgdColor = (BlockParity) ?
+                ((CellParity) ? EvenEvenColor : EvenOddColor) :
+                ((CellParity) ? OddEvenColor : OddOddColor);
+                [BkgdColor set];
+                
+                // The shape
+                NSUInteger HorizOrigin = ic*BDCellSize + BDCellMargin;
+                NSUInteger VertOrigin = ir*BDCellSize + BDCellMargin;
+                NSRect BkgdRect = NSMakeRect(HorizOrigin, VertOrigin, BDCellSize, BDCellSize);
+                NSBezierPath *BkgdPath = [NSBezierPath bezierPathWithRect:BkgdRect];
+                [BkgdPath fill];
+            }
+            
+            // The cell boundary lines
+            // Thick for major and thin for minor
+            [LineColor set];
+            for (int i=0; i<=SideLength; i++)
+            {
+                NSRect LineRect;
+                NSUInteger LWFac;
+                NSUInteger LineOrigin, LineWidth, LineOffset, LineLength;
+                NSBezierPath *LinePath;
+                
+                // Horizontal
+                LWFac = ((i % Rows) == 0) ? 2 : 1;
+                LineOrigin = BDCellMargin - LWFac;
+                LineOffset = LineOrigin + i*BDCellSize;
+                LineWidth = 2*LWFac;
+                LineLength = SideLength*BDCellSize + LineWidth;
+                LineRect = NSMakeRect(LineOrigin,LineOffset,LineLength,LineWidth);
+                LinePath = [NSBezierPath bezierPathWithRect:LineRect];
+                [LinePath fill];
+                
+                // Vertical
+                LWFac = ((i % Cols) == 0) ? 2 : 1;
+                LineOrigin = BDCellMargin - LWFac;
+                LineOffset = LineOrigin + i*BDCellSize;
+                LineWidth = 2*LWFac;
+                LineLength = SideLength*BDCellSize + LineWidth;
+                LineRect = NSMakeRect(LineOffset,LineOrigin,LineWidth,LineLength);
+                LinePath = [NSBezierPath bezierPathWithRect:LineRect];
+                [LinePath fill];
+            }
+        }
+        return YES;
+    };
     
-    // Draw!
-    for (uint ir=0; ir<SideLength; ir++)
-    {
-        // The cell backgrounds
-        for (uint ic=0; ic<SideLength; ic++)
-        {
-            // the coordinates
-            uint ibr = ir / Rows;
-            uint ibc = ic / Cols;
-            
-            // The color
-            bool CellParity = ((ir+ic) % 2) == 0;
-            bool BlockParity = ((ibr+ibc) % 2) == 0;
-            NSColor *BkgdColor = (BlockParity) ?
-            ((CellParity) ? EvenEvenColor : EvenOddColor) :
-            ((CellParity) ? OddEvenColor : OddOddColor);
-            [BkgdColor set];
-            
-            // The shape
-            NSUInteger HorizOrigin = ic*BDCellSize + BDCellMargin;
-            NSUInteger VertOrigin = ir*BDCellSize + BDCellMargin;
-            NSRect BkgdRect = NSMakeRect(HorizOrigin, VertOrigin, BDCellSize, BDCellSize);
-            NSBezierPath *BkgdPath = [NSBezierPath bezierPathWithRect:BkgdRect];
-            [BkgdPath fill];
-        }
-        
-        // The cell boundary lines
-        // Thick for major and thin for minor
-        [LineColor set];
-        for (int i=0; i<=SideLength; i++)
-        {
-            NSRect LineRect;
-            NSUInteger LWFac;
-            NSUInteger LineOrigin, LineWidth, LineOffset, LineLength;
-            NSBezierPath *LinePath;
-            
-            // Horizontal
-            LWFac = ((i % Rows) == 0) ? 2 : 1;
-            LineOrigin = BDCellMargin - LWFac;
-            LineOffset = LineOrigin + i*BDCellSize;
-            LineWidth = 2*LWFac;
-            LineLength = SideLength*BDCellSize + LineWidth;
-            LineRect = NSMakeRect(LineOrigin,LineOffset,LineLength,LineWidth);
-            LinePath = [NSBezierPath bezierPathWithRect:LineRect];
-            [LinePath fill];
-            
-            // Vertical
-            LWFac = ((i % Cols) == 0) ? 2 : 1;
-            LineOrigin = BDCellMargin - LWFac;
-            LineOffset = LineOrigin + i*BDCellSize;
-            LineWidth = 2*LWFac;
-            LineLength = SideLength*BDCellSize + LineWidth;
-            LineRect = NSMakeRect(LineOffset,LineOrigin,LineWidth,LineLength);
-            LinePath = [NSBezierPath bezierPathWithRect:LineRect];
-            [LinePath fill];
-        }
-    }
-    // All done with drawing
-    [BkgdImg unlockFocus];
-
+    // Use offscreen buffer
+    NSImage *BkgdImg = [NSImage imageWithSize:NSMakeSize(BoardSideLen, BoardSideLen)
+                                             flipped:YES drawingHandler:BackgroundDrawer];
     
     // Use this background image
     self.layer.contents = BkgdImg;
